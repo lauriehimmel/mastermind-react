ab24edf255d2b64ba84e7ada6feac3b164adc787
lauriehimmel <lauriehimmel1@gmail.com>
2023-11-15 19:25:24 -0500

    beginning setup, organizing code

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export default function Header () {
+    return (
+        <div className='"Header'>
+        <div className="header-title">Mastermind</div>
+        <div className="header-subtitle">Break the code, save the world.</div>
+        </div>
+    )
+}
---
+++ src/Gamepage.jsx
@@ -0,0 +1,9 @@
+import Header from "./Components/Header";
+
+export default function Gamepage () {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
