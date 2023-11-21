022e68a3e96a3de39c78df3d48ad2b2f25bfa3f1
lauriehimmel <lauriehimmel1@gmail.com>
2023-11-20 19:16:53 -0500

    adding navigate

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom'
 import './Components.css'
 
 export default function Header () {
+    const navigate = useNavigate();
+
     return (
         <div className='"Header'>
-        <div className="header-title">Mastermind</div>
+        <div className="header-title link"  onClick={() => navigate("/")}>Mastermind</div>
         <div className="header-subtitle">Break the code, save the world.</div>
         </div>
     )
--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
 import Instructions from "../Components/Instructions";
 
+
 export default function Welcome() {
+    const navigate = useNavigate();
     return (
         <>
-        <Header />
-        <div>Play</div>
+        <Header/>
+        <div onClick={() => navigate("/play")} className="link">Play</div>
         <div>Instructions</div>
         </>
     )
